d26700655c402f20ead8046a9a0d1b9d8863ee7a
zaqiell <130805286+zaqiell@users.noreply.github.com>
2023-06-26 18:15:56 +0000

    Add files via upload
    düzeltildi ve türkçe karakterler kaldırıldı

--- telegrambotapi/telegrambot.sh
+++ telegrambotapi/telegrambot.sh
@@ -2,38 +2,37 @@
 #!/bin/bash
 export LANG=C.UTF-8
 
-#TIRNAKLARI SİLİP GİRİNİZ#
-id = "botun idsini giriniz"
-token = "tırnakları silip giriniz"
+#tirnaklari silip giriniz#
+id="token"
+token="token"
 #****************************#
 
+tam=$id:$token
 
-
-echo 1\. fotoğraf göndermek istiyorum
+echo 1\. fotograf gondermek istiyorum
 echo 2\. text && read allah
 
 if [ $allah = 1 ]
 then
 	echo grup idsi gir: && read a
-	echo gönderilecek resim: && read f
-	echo kaç tane gönerilsin: && read c
-	b=bot6100611354:AAEzorOr2l1y0p5Pt2GDFZ-cynEWWdjNIFw
+	echo gonderilecek resim: && read f
+	echo kac tane gonderilsin: && read c
 	d=1
 	for (( d ; d<=c  ; d++ ))
 	do
-		curl -X POST -H "Content-Type:multipart/form-data" -F chat_id="$a" -F photo=@/home/anon$f "https://api.telegram.org/$b/sendPhoto"
+		curl -X POST -H "Content-Type:multipart/form-data" -F chat_id="$a" -F photo=@$f "https://api.telegram.org/bot$tam/sendPhoto"
 	done
 fi
 
 if [ $allah = 2 ]
 then
 	echo Grup id\'si giriniz: && read a
-	echo Göndereceğiniz mesajı giriniz: && read b
-	echo Kaç tane göndereceğinizi giriniz: && read c
+	echo Gondereceginiz mesaji giriniz: && read b
+	echo Kac tane gondereceginizi giriniz: && read c
 
 	i=0
 	for (( i ; i<c ; i++ ))
 	do
-        	curl --data chat_id="$a" --data-urlencode "text=$b" "https://api.telegram.org/bot$id:$token/sendMessage?parse_mode=HTML"
+        	curl --data chat_id="$a" --data-urlencode "text=$b" "https://api.telegram.org/bot$tam/sendMessage?parse_mode=HTML"
 	done
 fi
